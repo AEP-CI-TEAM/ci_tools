8629ac5c394b7d98c7f72a771fd82683ba022c62
silabs-CongD <114218374+silabs-CongD@users.noreply.github.com>
2025-04-10 15:14:33 +0700

    Create sl_tflite_micro_model_parameters.h

---
+++ sl_tflite_micro_model_parameters.h
@@ -0,0 +1,16 @@
+// Auto-generated parameters from TFLite flatbuffers in config directory
+#ifndef SL_TFLITE_MICRO_MODEL_PARAMETERS_H
+#define SL_TFLITE_MICRO_MODEL_PARAMETERS_H
+
+// Definitions generated from "thumbs_up_and_down.tflite"'
+#define SL_TFLITE_MODEL_CLASSES {"nothing", "thumbs_up", "thumbs_down"}
+#define SL_TFLITE_MODEL_DATE "2022-04-28T06:35:08.801Z"
+#define SL_TFLITE_MODEL_HASH "56g42674b4587fh5tb3b3e20048ca7fg"
+#define SL_TFLITE_MODEL_NAME "thumbs_up_and_down"
+#define SL_TFLITE_MODEL_RUNTIME_MEMORY_SIZE 7296
+#define SL_TFLITE_MODEL_SAMPLEWISE_NORM_MEAN_AND_STD true
+#define SL_TFLITE_MODEL_SAMPLEWISE_NORM_RESCALE 0.0f
+#define SL_TFLITE_MODEL_VERSION 1
+
+
+#endif // SL_TFLITE_MICRO_MODEL_PARAMETERS_H
